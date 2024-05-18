6151cf515ea5b553c357a8479d063af336f2b745
Takashi-Matsumura <mats@coral.plala.or.jp>
2024-05-18 17:56:09 +0900

    1st github commit

---
+++ app/components/UserSelect.tsx
@@ -0,0 +1,44 @@
+import {
+  FormControl,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+} from "@mui/material";
+
+type CustomSelectProps = {
+  user: User;
+  setUser: React.Dispatch<React.SetStateAction<User>>;
+  users: User[];
+};
+
+const UserSelect: React.FC<CustomSelectProps> = ({ user, setUser, users }) => {
+  const handleChange = (event: SelectChangeEvent) => {
+    const selectedUser = users.find(
+      (user) => user?.name === event.target.value
+    );
+    if (selectedUser) setUser(selectedUser);
+  };
+
+  return (
+    <div>
+      <div>
+        <p>名前を選択してください</p>
+        <FormControl fullWidth>
+          <Select
+            className="text-3xl"
+            onChange={handleChange}
+            value={user ? user.name : ""}
+          >
+            {users.map((user: User) => (
+              <MenuItem key={user?.id} value={user?.name}>
+                {user?.name}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
+    </div>
+  );
+};
+
+export default UserSelect;
